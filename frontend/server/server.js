d64081c16db4243127f3e0a30ba16e5673b0e42b
BlazeSN860 <virajprabhup@gmail.com>
2023-10-21 21:15:24 +0530

    fix(Gradient): Changed maroon to yellow gradient.

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -2,7 +2,28 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+// const nodemailer = require('nodemailer');
 
+// var transport = nodemailer.createTransport({
+//     host: "sandbox.smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//         user: "cfcce67ea424bb",
+//         pass: "1e92e1e2b3c06a"
+//     }
+// });
+// const send = async options => {
+//     const message = {
+//         from: "Nodemailer <example@nodemailer.com>",
+//         to: "virajprabhup@gmail.com",
+//         subject: "Thankyou for contacting us",
+//         text: "We will get back to you soon"
+//     };
+
+//     const info = await transporter.sendMail(message);
+//     console.log(info.messageId);
+//     return info;
+// }
 
 app.use('/css', express.static('../public/css'));
 app.use('/js', express.static('../public/js'));
@@ -26,6 +47,32 @@ app.get('/doctors', (req, res) => {
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'contact.html'));
 });
+
+// app.post('/api/send-email', async (req, res) => {
+//     try {
+//         const response = await send(req);
+//         console.log(response);
+//         res.status(200).json({
+//             message: 'sucess',
+//             data: response
+//         })
+//     } catch (error) {
+//         console.log(error);
+//         res.status(400).json({
+//             message: error.message
+//         })
+//     }
+// })
+
+
+/*
+Use in your req.body
+ const options = {
+   userEmail: <mailtrapEmail>,
+   subject: 'Welcome to Auffr',
+   message: 'We are excited to have you in the family'
+ }
+*/
 
 
 
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,5 @@
 require('bootstrap'); // Import Bootstrap JavaScript (including Popper.js)
-
+import axios from 'axios';
 var cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
@@ -12,3 +12,28 @@ cards.forEach(card => {
         tooth.classList.remove('tooth1');
     })
 })
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const form = document.getElementById("contact-form");
+
+//     form.addEventListener("submit", function (event) {
+//         event.preventDefault();
+
+//         const formData = new FormData(form);
+//         var object = {};
+//         formData.forEach(function (value, key) {
+//             object[key] = value;
+//         });
+//         object["subject"] = "A new Query from " + object.name + " through the Dental website";
+//         console.log(object);
+//         axios.post("/api/send-email", object)
+//             .then(function (response) {
+//                 console.log("Email sent:", response.data);
+//                 // Handle success, e.g., show a success message to the user
+//             })
+//             .catch(function (error) {
+//                 console.error("Error sending email:", error);
+//                 // Handle error, e.g., show an error message to the user
+//             });
+//     });
+// }); 
